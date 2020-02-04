fixed a bug in normalization of 'fedavg-ic', found a rationale for using the 'mu' normalization method

algorithm/fedavg-ic-abc.py
@@ -224,7 +224,7 @@ class Algorithm(AbstractAlgorithm):
         return (c_star, delta_star, cntSteady, cntPrint)
     
     def runGrouping(self, c_edge, nid2_g_i__w, g__w):
-        print('IID Grouping Started')
+        print('Grouping Started')
         
         c = Cloud(c_edge.topology, c_edge.D_byNid, self.args.numGroups)
         z_rand = fl_data.groupRandomly(self.args.numNodes, self.args.numGroups)
@@ -241,7 +241,7 @@ class Algorithm(AbstractAlgorithm):
         print('Final cost_star=%.3f, numGroups=%d, d_group=%.3f, d_global=%.3f' % \
               (cost_star, len(c_star.groups), d_group, d_global))
         
-        print('IID Grouping Finished')
+        print('Grouping Finished')
         return c_star
     
     def runKMedoidsGrouping(self, c, nid2_g_i__w, g__w):
@@ -249,19 +249,25 @@ class Algorithm(AbstractAlgorithm):
         # https://en.wikipedia.org/wiki/K-medoids
         
         # Initialize Medoids
-        medoidNids = []
-        cntEid = 0
-        for gid in range(self.args.numGroups):
-            nidsInEdge = c.topology.getNids(cntEid % self.args.numEdges)
-            while True:
-                randNid = np.random.choice(nidsInEdge)
-                if randNid in medoidNids:
-                    continue
-                else:
-                    medoidNids.append(randNid)
-                    break
-            cntEid += 1
-#         medoidNids = np.random.choice(np.arange(self.args.numNodes), size=self.args.numGroups, replace=False)
+#         medoidNids = []
+#         cntEid = 0
+#         for gid in range(self.args.numGroups):
+#             nidsInEdge = c.topology.getNids(cntEid % self.args.numEdges)
+#             while True:
+#                 randNid = np.random.choice(nidsInEdge)
+#                 if randNid in medoidNids:
+#                     continue
+#                 else:
+#                     medoidNids.append(randNid)
+#                     break
+#             cntEid += 1
+        medoidNids = np.random.choice(np.arange(self.args.numNodes), size=self.args.numGroups, replace=False)
+
+        # Initialize Group Membership of Medoids
+#         z = fl_data.to_z(self.args.numNodes, c.nids_byGid)
+#         for k, medoidNid in enumerate(medoidNids):
+#             z[medoidNid] = k
+            
         print('Initial medoidNids:', sorted(medoidNids))
         
         # Associate
@@ -298,37 +304,37 @@ class Algorithm(AbstractAlgorithm):
         c = c.clone(nid2_g_i__w, g__w)
         medoidNids_byGid = [ [nid] for nid in medoidNids ]
         if c.digest(medoidNids_byGid, nid2_g_i__w, g__w) == False: raise Exception(str(medoidNids_byGid))
-        
+            
         # Shuffle index every iteration
         randNids = np.arange(self.args.numNodes)
         np.random.shuffle(randNids)
         
         z = fl_data.to_z(self.args.numNodes, c.nids_byGid)
-        for i in randNids:
+        for nid in randNids:
             # Medoid 일 경우 무시
-            if i in medoidNids: continue
+            if nid in medoidNids: continue
                 
             # Search for candidates with the same minimum cost
             costs = []
-            for k, g in enumerate(c.groups):
+            for k, medoidNid in enumerate(medoidNids):
                 # 목적지 그룹이 이전 그룹과 같을 경우 무시
-                if z[i] == k: continue
+                if z[nid] == k: continue
                     
                 # 그룹 멤버쉽 변경 시도
-                z[i] = k
+                z[nid] = k
                 
                 # Cloud Digest 시도
                 nids_byGid = fl_data.to_nids_byGid(z)
                 if c.digest(nids_byGid, nid2_g_i__w, g__w) == False: raise Exception(str(z), str(nids_byGid))
                     
                 # 다음 후보에 대한 Cost 계산
-                costs.append(self.getAssociateCost(c))
+                costs.append(self.getAssociateCost(c, nid, medoidNid))
             costs = np.array(costs)
             min_ks = np.where(costs == costs.min())[0]
             min_k = np.random.choice(min_ks)
-            z[i] = min_k # 다음 Iteration 에서 Digest
+            z[nid] = min_k # 다음 Iteration 에서 Digest
             cost_min = costs[min_k]
-            print('nid: %3d\tcurrent cost: %.3f' % (i, cost_min))
+            print('nid: %3d\tcurrent cost: %.3f' % (nid, cost_min))
             
         # 마지막 멤버십 초기화가 발생했을 수도 있으므로, Cloud Digest 시도
         nids_byGid = fl_data.to_nids_byGid(z)
@@ -337,13 +343,13 @@ class Algorithm(AbstractAlgorithm):
         for k, g in enumerate(c.groups):
             p_k = c.get_p_ks()[k]
             print(g.ps_nid, g.get_N_k(), p_k*g.get_DELTA_k())
-        print('DELTA: ', c.get_DELTA())
-        return c, cost_min
+        print('DELTA: ', c.get_DELTA(), c.get_sum_hpp_group(False))
+        return c, cost_min #self.getAssociateFinalCost(c)
     
     @abstractmethod
-    def getAssociateCost(self, c):
+    def getAssociateCost(self, c, nid, medoidNid):
         pass
-
+    
     @abstractmethod
     def determineMedoidNids(self, c, nid2_g_i__w, g__w):
         pass

algorithm/fedavg-ic.py
@@ -10,27 +10,12 @@ FedavgIcBaseAlgorithm = getattr(FedavgIcBaseModule, 'Algorithm')
 
 GROUPING_MAX_STEADY_STEPS = 1
 
-NUM_NORM_CONST_SAMPLES = 50
-
-# def removeOutliers(x, outlierConstant=1.5):
-#     a = np.array(x)
-#     upper_quartile = np.percentile(a, 75)
-#     lower_quartile = np.percentile(a, 25)
-#     IQR = (upper_quartile - lower_quartile) * outlierConstant
-#     quartileSet = (lower_quartile - IQR, upper_quartile + IQR)
-#     resultList = []
-#     for y in a.tolist():
-#         if y >= quartileSet[0] and y <= quartileSet[1]:
-#             resultList.append(y)
-#     return resultList
+NUM_NORM_CONST_SAMPLES = 100
+NC_TYPE = 'mu' # mu, min-max, z-score
+# min-max are z-score are not appropriate for the normalization of multi-objective optimization
+# because minus value may occur and interfere with cost search process
 
 class Algorithm(FedavgIcBaseAlgorithm):
-    
-#     def __init__(self, args):
-#         super().__init__(args)
-#         default_linkSpeed = args.linkSpeeds[0]
-#         self.nid2delay = self.c.topology.profileDelay(1024, default_linkSpeed)
-#         print('Group Delay', self.c.get_delay_group(self.nid2delay))
         
     def runGrouping(self, c_edge, nid2_g_i__w, g__w):
         print('Comm-IID Grouping Started')
@@ -43,37 +28,68 @@ class Algorithm(FedavgIcBaseAlgorithm):
         cntSteady = 0
         numGroups = self.args.numGroups # numGroups = 1 일 때는, DELTA 가 0 이 되므로 다른 numGroups 경우와 Cost 비교 불가
         while cntSteady < GROUPING_MAX_STEADY_STEPS and numGroups <= self.args.numGroups:
+            # Cost Normalization Constant 샘플링
+            iid_costs = [] ; comm_final_costs = []
+            iid_k_costs = [] ; comm_k_costs = []
+            for _ in range(NUM_NORM_CONST_SAMPLES):
+                c_sample = Cloud(c_edge.topology, c_edge.D_byNid, numGroups)
+                
+                # Get Global Associate Cost
+                medoidNids = np.random.choice(np.arange(self.args.numNodes), size=numGroups, replace=False)
+                medoidNids_byGid = [ [nid] for nid in medoidNids ]
+                if c_sample.digest(medoidNids_byGid, nid2_g_i__w, g__w) == False: raise Exception(str(medoidNids_byGid))
+                    
+                randNids = np.arange(self.args.numNodes)
+                np.random.shuffle(randNids)
+                
+                z = fl_data.to_z(self.args.numNodes, c_sample.nids_byGid)
+                for nid in randNids:
+                    if nid in medoidNids: continue
+                        
+                    k = np.random.randint(numGroups)
+                    z[nid] = k
+                    nids_byGid = fl_data.to_nids_byGid(z)
+                    if c_sample.digest(nids_byGid, nid2_g_i__w, g__w) == False: raise Exception(str(nids_byGid))
+                    iid_costs.append(c_sample.get_DELTA())
+                    comm_final_costs.append(c_sample.get_sum_hpp_group(False))
+                    
+                # Get Group Medoid Cost
+                for g in c_sample.groups:
+                    N_k = g.get_N_k()
+                    costs = []
+                    for nid1 in N_k:
+                        p_i = c_sample.get_p_is()[nid1]
+                        iid_i = np.linalg.norm(nid2_g_i__w[nid1] - g__w)
+                        comm_i = sum( c_sample.topology.getDistance(nid1, nid2) for nid2 in N_k )
+                        iid_k_costs.append(p_i*iid_i)
+                        comm_k_costs.append(comm_i)
+            self.nc_iid_mu = np.mean(iid_costs)
+            self.nc_iid_std = np.std(iid_costs)
+            self.nc_iid_min = min(iid_costs)
+            self.nc_iid_max = max(iid_costs)
+            
+            self.nc_comm_mu = np.mean(comm_final_costs)
+            self.nc_comm_std = np.std(comm_final_costs)
+            self.nc_comm_min = min(comm_final_costs)
+            self.nc_comm_max = max(comm_final_costs)
+            
+            self.nc_iid_k_mu = np.mean(iid_k_costs)
+            self.nc_iid_k_std = np.std(iid_k_costs)
+            self.nc_iid_k_min = min(iid_k_costs)
+            self.nc_iid_k_max = max(iid_k_costs)
+            
+            self.nc_comm_k_mu = np.mean(comm_k_costs)
+            self.nc_comm_k_std = np.std(comm_k_costs)
+            self.nc_comm_k_min = min(comm_k_costs)
+            self.nc_comm_k_max = max(comm_k_costs)
+            print('Cost Norm Constant:', self.nc_iid_min, self.nc_iid_max, self.nc_comm_min, self.nc_comm_max, \
+                  self.nc_iid_k_min, self.nc_iid_k_max, self.nc_comm_k_min, self.nc_comm_k_max)
+            
             print('numGroups:', numGroups)
             c_cur = Cloud(c_edge.topology, c_edge.D_byNid, numGroups)
             z_rand = fl_data.groupRandomly(self.args.numNodes, numGroups)
             nids_byGid = fl_data.to_nids_byGid(z_rand)
             c_cur.digest(nids_byGid, nid2_g_i__w, g__w)
-            
-            # Cost Normalization Constant 샘플링
-            iid_costs = [] ; comm_costs = []
-            iid_k_costs = [] ; comm_k_costs = []
-            for _ in range(NUM_NORM_CONST_SAMPLES):
-                c_sample = Cloud(c_edge.topology, c_edge.D_byNid, numGroups)
-                z_rand = fl_data.groupRandomly(self.args.numNodes, numGroups)
-                nids_byGid = fl_data.to_nids_byGid(z_rand)
-                c_sample.digest(nids_byGid, nid2_g_i__w, g__w)
-                
-                iid_costs.append(c_sample.get_DELTA())
-                comm_costs.append(c_sample.get_sum_hpp_group(False))
-                
-                for g in c_sample.groups:
-                    N_k = g.get_N_k()
-                    costs = []
-                    for nid1 in N_k:
-                        iid_k_costs.append(np.linalg.norm(nid2_g_i__w[nid1] - g__w))
-                        comm_k_costs.append(sum( c_sample.topology.getDistance(nid1, nid2) for nid2 in g.N_k ))
-            self.norm_const_iid = np.mean(iid_costs)
-            self.norm_const_comm = np.mean(comm_costs)
-            self.norm_const_k_iid = np.mean(iid_k_costs)
-            self.norm_const_k_comm = np.mean(comm_k_costs)
-            print('Cost Norm Constant:', min(iid_costs), max(iid_costs), min(comm_costs), max(comm_costs), \
-                  min(iid_k_costs), max(iid_k_costs)), min(comm_k_costs), max(comm_k_costs)
-            
             c_cur, cost_cur = self.runKMedoidsGrouping(c_cur, nid2_g_i__w, g__w)
             if self.isGreaterThan(cost_star, cost_cur):
                 c_star, cost_star = c_cur, cost_cur
@@ -87,18 +103,24 @@ class Algorithm(FedavgIcBaseAlgorithm):
         d_global = self.getCommTimeGlobal(c_star, self.model.size, default_linkSpeed)
         self.setDefaultCommDelay(d_group, d_global)
         
-        print('Final cost_star=%.3f, numGroups=%d, d_group=%.3f, d_global=%.3f' % \
-              (cost_star, len(c_star.groups), d_group, d_global))
-        print(c_star.get_DELTA()/self.norm_const_iid, c_star.get_sum_hpp_group(False)/self.norm_const_comm)
-        
+        print('Final cost_star=%.3f, numGroups=%d, d_group=%.3f, d_global=%.3f' % (cost_star, len(c_star.groups), d_group, d_global))
         print('Comm-IID Grouping Finished')
         return c_star
     
-    def getAssociateCost(self, c):
-        if self.norm_const_comm == 0 or self.norm_const_iid == 0:
-            raise Exception(str(self.norm_const_iid), str(self.norm_const_comm))
-        return c.get_DELTA()/self.norm_const_iid + c.get_sum_hpp_group(False)/self.norm_const_comm
-    
+    def getAssociateCost(self, c, nid, medoidNid):
+        if self.nc_comm_mu == 0 or self.nc_iid_mu == 0:
+            raise Exception(str(self.nc_iid_mu), str(self.nc_comm_mu))
+        cost_iid = c.get_DELTA() ; cost_comm = c.get_sum_hpp_group(False)
+        if NC_TYPE == 'mu':
+            return cost_iid/self.nc_iid_mu + cost_comm/self.nc_comm_mu
+        elif NC_TYPE == 'min-max':
+            return (cost_iid-self.nc_iid_min)/(self.nc_iid_max-self.nc_iid_min) \
+                    + (cost_comm-self.nc_comm_min)/(self.nc_comm_max-self.nc_comm_min)
+        elif NC_TYPE == 'z-score':
+            return (cost_iid-self.nc_iid_mu)/self.nc_iid_std + (cost_comm-self.nc_comm_mu)/self.nc_comm_std
+        else:
+            raise Exception(NC_TYPE)
+            
     def determineMedoidNids(self, c, nid2_g_i__w, g__w):
         medoidNids = []
         for g in c.groups:
@@ -107,7 +129,15 @@ class Algorithm(FedavgIcBaseAlgorithm):
             for nid1 in N_k:
                 p_i = c.get_p_is()[nid1]
                 iid_i = np.linalg.norm(nid2_g_i__w[nid1] - g__w)
-                comm_i = sum( c.topology.getDistance(nid1, nid2) for nid2 in g.N_k )
-                costs.append( p_i*iid_i/self.norm_const_k_iid + comm_i/self.norm_const_k_comm )
+                comm_i = sum( c.topology.getDistance(nid1, nid2) for nid2 in N_k )
+                if NC_TYPE == 'mu':
+                    costs.append( p_i*iid_i/self.nc_iid_k_mu + comm_i/self.nc_comm_k_mu )
+                elif NC_TYPE == 'min-max':
+                    costs.append( (p_i*iid_i-self.nc_iid_k_min)/(self.nc_iid_k_max-self.nc_iid_k_min) \
+                                 + (comm_i-self.nc_comm_k_min)/(self.nc_comm_k_max-self.nc_comm_k_min) )
+                elif NC_TYPE == 'z-score':
+                    costs.append( (p_i*iid_i-self.nc_iid_k_mu)/self.nc_iid_k_std + (comm_i-self.nc_comm_k_mu)/self.nc_comm_k_std )
+                else:
+                    raise Exception(NC_TYPE)
             medoidNids.append(N_k[ np.argmin(costs) ])
         return medoidNids